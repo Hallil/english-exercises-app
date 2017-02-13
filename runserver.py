76ab638bbb8d0629797cf375000417c0abb36626
LambdaNL <rene.van.der.horst@protonmail.com>
2017-02-13 18:49:57 +0100

    app removal

--- runserver.py
+++ runserver.py
@@ -3,9 +3,30 @@ This script runs the webshop application using a development server.
 """
 
 from os import environ
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "English Exercises :DDDDDD"
+
+# Section for Rene
+@app.route("/adverbs")
+def adverbs():
+    return "Adverbs Exercises :DDDDDD"
+
+# Section for Eelco
+@app.route("/gerund")
+def gerund():
+    return "Gerund Exercises :DDDDDD"
+
+# Section for Halil
+@app.route("/nouns")
+def nouns():
+    return "Nouns Exercises :DDDDDD"
 
 if __name__ == '__main__':
-    app = Flask(__name__)
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
