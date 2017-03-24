4e90f4afc10138deff923a6064a441a2d5cdf845
Halil Bilen <halilbilen@live.nl>
2017-03-25 00:40:18 +0100

    Testing

--- english_exercises/db_layer.py
+++ english_exercises/db_layer.py
@@ -1,7 +1,6 @@
 from english_exercises import db
-from english_exercises.models import User, OpenQuestion
+from english_exercises.models import User, OpenQuestion, MultiQuestion
 from english_exercises.level_access import calculate_score
-
 
 def correct_answers_in_post(request):
     correct_answer_count = 0
@@ -11,6 +10,13 @@ def correct_answers_in_post(request):
             correct_answer_count += 1
     return correct_answer_count
 
+def correct_answers_in_post_multi(request):
+    correct_answer_count_multi = 0
+    for key, value in request.items():
+        correct_answer_multi = find_answer_multiObject(key)
+        if correct_answer_multi.answer & correct_answer_multi.multiplechoiceAnswer == value:
+                correct_answer_count_multi += 1
+    return correct_answer_count_multi
 
 def incorrect_answers_in_post(request):
     incorrect_answer_count = 0
@@ -20,10 +26,19 @@ def incorrect_answers_in_post(request):
             incorrect_answer_count += 1
     return incorrect_answer_count
 
+def incorrect_answers_in_post_multi(request):
+    incorrect_answer_count_multi = 0
+    for key, value in request.items():
+        correct_answer_multi = find_answer_multiObject(key)
+        if correct_answer_multi.answer & correct_answer_multi.multiplechoiceAnswer == value:
+                incorrect_answer_count_multi += 1
+    return incorrect_answer_count_multi
 
 def find_answer_object(question_id):
     return OpenQuestion.query.filter_by(id=question_id).first()
 
+def find_answer_multiObject(question_id):
+    return MultiQuestion.query.filter_by(id=question_id).first()
 
 def update_user_results(user_name, correct, incorrect):
     user = User.query.filter_by(username=user_name).first()
